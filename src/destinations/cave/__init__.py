a60855624be0bcc1980b3c4fd6ca9d298b90fd66
Priccila <priccilalucem@gmail.com>
2024-12-06 16:51:34 -0300

    fix: inside cave actions after been defeated by owlbear

--- src/destinations/cave/__init__.py
+++ src/destinations/cave/__init__.py
@@ -40,6 +40,26 @@ def cave(owl_bear_cave: OwlBearCave, main_character: Hero, stdscr: curses.window
 
 
 def inside_the_cave(stdscr: curses.window, main_character: Hero, owl_bear_cave: OwlBearCave) -> None:
+    combat_done = False
+
+    def npc_interactions():
+        while not should_exit():
+            try:
+                stdscr.clear()
+                stdscr.addstr(inside_cave_commands())  
+                stdscr.refresh()
+                
+                owl_bear_cave_key = stdscr.getch()
+                action = actions.get(
+                    chr(owl_bear_cave_key).upper(),
+                    lambda: display_message(stdscr, "Invalid choice. Try again.", 1000)
+                )
+                action()
+            except StopIteration:
+                break
+            except Exception as e:
+                display_message(stdscr, f"An error occurred: {e}", 2000)
+
     actions = {
         "S": lambda: main_character.show_status(stdscr),
         "E": lambda: display_message(stdscr, "Returning to outside the cave...", 1000) or exit_loop(),
@@ -48,8 +68,6 @@ def inside_the_cave(stdscr: curses.window, main_character: Hero, owl_bear_cave: 
         "3": lambda: display_message(stdscr, owl_bear_cave.owl_bear.drop_items(main_character), 3000),
         chr(27): lambda: display_message(stdscr, "Exiting the game...", 1000) or set_exit(),
     }
-
-    combat_done = False
 
     while not should_exit():
         if not combat_done and any(quest.id == 1 for quest in main_character.quests):
@@ -63,35 +81,20 @@ def inside_the_cave(stdscr: curses.window, main_character: Hero, owl_bear_cave: 
 
             if not combat(stdscr, main_character, owl_bear_cave.owl_bear):
                 display_message(stdscr, the_real_init(), 3000)
-                main_character.choose_character_class(stdscr)                
+                main_character.choose_character_class(stdscr)
 
                 display_message(stdscr, (
                     "You wake up to Damon’s brothers frantically trying to wake you up. "
                     "As you look to the side, you see the lifeless body of the OwlBear and wonder how it all happened..."
                 ), 5000)
 
-                main_character.health_points = 50  
+                main_character.health_points = main_character.max_hp
                 quest_to_remove = next((quest for quest in main_character.quests if quest.id == 1), None)
                 if quest_to_remove:
                     main_character.conclude_quests(quest_to_remove)
                     main_character.quests.remove(quest_to_remove)
 
-                try:
-                    stdscr.clear()
-                    stdscr.addstr(inside_cave_commands())  
-                    stdscr.refresh()
-
-                    owl_bear_cave_key = stdscr.getch()
-                    action = actions.get(
-                        chr(owl_bear_cave_key).upper(),
-                        lambda: display_message(stdscr, "Invalid choice. Try again.", 1000)
-                    )
-                    action()
-
-                except StopIteration:
-                    break 
-                except Exception as e:
-                    display_message(stdscr, f"An error occurred: {e}", 2000)
+                npc_interactions() 
         else:
             display_message(stdscr, "Not ready, come back later!", 2000)
             break
